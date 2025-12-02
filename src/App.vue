c76df4c39f9a59bd29f366712591b8cc33dfd8a7
matthieu.dehondt <matthieu.dehondt@gmail.com>
2025-12-02 09:08:41 +0000

    install tailwind, made layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="mastermind grid lg:grid-cols-3 lg:grid-rows-2 gap-4">
+        <div class="info order-1">1</div>
+        <div class="commands order-3 lg:order-2 lg:order-none bg-red-500 lg:row-start-2">2</div>
+        <div class="board order-2 lg:order-3 lg:col-start-2 lg:col-span-2 lg:row-span-2">3</div>
+    </div>
 </template>
 
+<script setup lang="ts">
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
